3421cffa5cb4214a2a5859ae3b054f80f77c05d9
Bud Cribar <budcribar@msn.com>
2025-06-29 16:05:49 -0600

    update TS client generator for Any wrappers

--- src/demo/TypeScriptMonsterClicker/GameViewModelRemoteClient.ts
+++ src/demo/TypeScriptMonsterClicker/GameViewModelRemoteClient.ts
@@ -2,6 +2,8 @@
 import { GameViewModelServiceClient } from './generated/GameViewModelService_pb_service';
 import { GameViewModelState, UpdatePropertyValueRequest, SubscribeRequest } from './generated/GameViewModelService_pb';
 import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
+import { Any } from 'google-protobuf/google/protobuf/any_pb';
+import { StringValue, Int32Value, BoolValue } from 'google-protobuf/google/protobuf/wrappers_pb';
 
 export class GameViewModelRemoteClient {
     private readonly grpcClient: GameViewModelServiceClient;
@@ -38,12 +40,34 @@ export class GameViewModelRemoteClient {
     }
 
     async updatePropertyValue(propertyName: string, value: any): Promise<void> {
-        const req: UpdatePropertyValueRequest = { propertyName, newValue: value }; 
+        const req = new UpdatePropertyValueRequest();
+        req.setPropertyName(propertyName);
+        req.setNewValue(this.createAnyValue(value));
         await new Promise<void>((resolve, reject) => {
             this.grpcClient.updatePropertyValue(req, (err) => {
                 if (err) reject(err); else resolve();
             });
         });
+    }
+
+    private createAnyValue(value: any): Any {
+        const anyVal = new Any();
+        if (typeof value === 'string') {
+            const wrapper = new StringValue();
+            wrapper.setValue(value);
+            anyVal.pack(wrapper.serializeBinary(), 'google.protobuf.StringValue');
+        } else if (typeof value === 'number' && Number.isInteger(value)) {
+            const wrapper = new Int32Value();
+            wrapper.setValue(value);
+            anyVal.pack(wrapper.serializeBinary(), 'google.protobuf.Int32Value');
+        } else if (typeof value === 'boolean') {
+            const wrapper = new BoolValue();
+            wrapper.setValue(value);
+            anyVal.pack(wrapper.serializeBinary(), 'google.protobuf.BoolValue');
+        } else {
+            throw new Error('Unsupported value type');
+        }
+        return anyVal;
     }
 
 }
